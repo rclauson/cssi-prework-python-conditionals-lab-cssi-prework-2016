8a83629b8baf573f13af26e5fe6b904638af66d5
dfenjves <fenjves.daniel@gmail.com>
2016-03-11 10:40:03 -0500

    made changes to readme

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ In this lab you will write a program that gets your user ready for their day at 
 
 The functions are already written for you since we haven't quite covered Python functions yet. The general concepts should be familiar from JavaScript. Note that the raw_input() function returns whatever the user types and has a data type of string.
 
-To run the tests at any point as you work through your lab, make sure you are in the lab directory and then run the command `python morning_test.py`. If any test fails, you will understand what the test was checking.
+To run the tests at any point as you work through your lab, make sure you are in the lab directory and then run the command `python morning_test.py`. If any test fails, you will understand what the test was checking. Notice that we don't call the functions in `morning.py` - instead, we import the functions into `morning_test.py` and call them there in our tests. Make sure to look at `morning_test.py` before you get started!
 
 
 ##1. Waking Up
--- morning.py
+++ morning.py
@@ -1,49 +1,17 @@
-def isfloat(value):
-  try:
-    float(value)
-    return True
-  except:
-    return False
-
-
 #1. Waking Up
 def wake_up(day_of_week):
     #write your conditional statement here.
-
-    return message
+    return "replace this line with all of your code!"
 
 
 #2. The Commute
 def commute(climate, mins_until_work):
     #write your conditional statement here
+    return "replace this line with all of your code!"
 
-    return transport_type
 
 
 #3. Coffee Buzz
 def coffee(number_of_emails):
     #write your conditional statement here
-
-    return response
-
-
-#You do not need to modify anything below this comment
-
-#Asks user what day it is, calls wake_up() with that answer as an argument
-day_of_week = raw_input("What day is it?")
-print wake_up(day_of_week)
-
-#Calls commute() with two arguments
-weather = raw_input("What is the weather outside? (Rainy or Sunny)")
-mins = raw_input("How many minutes do you have before you're late?")
-print commute(weather,float(mins))
-
-#Checks if the user's input can be converted to a float, otherwise asks user to reenter their number
-emails = raw_input("You made it to work on time. How many unread emails are in your inbox?")
-if(isfloat(emails)):
-    #convert to an integer
-    email_num=float(emails)
-    #call the coffee function
-    print coffee(email_num)
-else:
-    emails = raw_input("Please enter a number!")
+    return "replace this line with all of your code!"
