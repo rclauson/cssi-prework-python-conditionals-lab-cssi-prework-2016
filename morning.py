39a6ea3f1554ee6f95a8b33d4e2d060e4c1d3532
dfenjves <fenjves.daniel@gmail.com>
2016-03-16 12:19:47 -0400

    merge

--- morning.py
+++ morning.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> 948bcd54b7c945b75c17a6e6d05df447c483bbc3
 #1. Waking Up
 def wake_up():
     #write your conditional statement here.
@@ -18,22 +15,4 @@ def commute():
 #3. Coffee Buzz
 def coffee():
     #write your conditional statement here
-<<<<<<< HEAD
     return "replace this line with all of your code!"
-<<<<<<< HEAD
-=======
-    if (number_of_emails>=100):
-        response = "Time for a triple shot espresso"
-    elif (number_of_emails>=50):
-        response = "Go for a large black coffee"
-    elif (number_of_emails>0):
-        response = "A latte will serve you well"
-    elif (number_of_emails==0):
-        response = "You have reached the Nirvana of 21st century communication!"
-    else:
-       response = "Is this a new Gmail feature?"
-    return response
->>>>>>> 948bcd54b7c945b75c17a6e6d05df447c483bbc3
-
-=======
->>>>>>> 96cd8fea1681441aa957366b39360a5000cfcd6a
