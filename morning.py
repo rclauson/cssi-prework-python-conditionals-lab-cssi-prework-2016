309bf3b922015ae6499843b0bd4f07e51ddac968
rclauson <rclauson@google.com>
2016-05-30 18:16:38 -0400

    Done.

--- morning.py
+++ morning.py
@@ -1,14 +1,32 @@
 
 #1. Waking Up
-def wake_up():
-    #write your conditional statement here.
-    return "replace this line with all of your code!"
+def wake_up(day_of_week):
+    if (day_of_week.upper() == "Saturday"):
+      instructions = "Go back to bed"
+    if (day_of_week.upper() == "Sunday"):
+      instructions = "Go back to bed"
+    else:
+      instructions =  "Stop hitting snooze"
+    return message
+
+  wake_up_request = raw_input("Do you have to wake up today?")
+  print wake_up(wake_up_request)
 
 
 #2. The Commute
-def commute():
-    #write your conditional statement here
-    return "replace this line with all of your code!"
+def commute(weather, mins_until_work):
+    if (weather) == "rainy"
+      instructions = "Better take the car"
+    if (mins_until_work) == <10+"mins"
+      instructions = "Better take the car"
+    if (weather) == "sunny"
+      instructions = "Enjoy a bike ride"
+    if (mins_until_work) == >10+"mins"
+      instructions = "Enjoy a bike ride"
+    return message
+
+  commute_request = raw_input ("How should I get to work?")
+  print commute(commute_request)
 
 
 
